589a632aad430928b9e20b8f5b2813c2ff3e98b3
Kyri Panayi <kpanayi7@gmail.com>
2022-01-20 20:53:46 +0000

    challenge completed

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,21 @@
 # This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the bin/rails db:seed command (or created alongside the database with db:setup).
+# The data can then be loaded with the rails db:seed command (or created alongside the database with db:setup).
 #
 # Examples:
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+puts 'Seeding started'
+4.times do
+  Flat.create!(
+    name: Faker::Space.galaxy,
+    address: Faker::Address.full_address,
+    description: Faker::Emotion.adjective,
+    price_per_night: rand(50..5000),
+    number_of_guests: rand(1..13)
+)
+  puts 'Flat created'
+end
+
+puts 'Seeding done'
